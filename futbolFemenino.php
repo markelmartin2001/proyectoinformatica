89dba940851128d8f8a589f6d0f9105ff6cf2bcd
markelmartin2001 <102917180+markelmartin2001@users.noreply.github.com>
2022-06-06 17:25:12 +0200

    Add files via upload

--- futbolFemenino.php
+++ futbolFemenino.php
@@ -18,10 +18,14 @@
 </head>
 <body>
 <?php require 'header.php'?>
+
+<div class = "contendor">
+
+
+
 <div class="titulo">
     <h1>FOTOS DEL ULTIMO PARTIDO</h1>
 </div>
-
 
 <div class="slider">
     <ul>
@@ -31,23 +35,77 @@
     <li><img src="Pictures/imagenslide4.jpg" alt=""></li>
 </ul>
 </div>
+</div> 
 
+<title>Resumenes de los ultimos partidos</title>
+<div class="contendorgrande">
+<div class="container">
+  <div class="card">
+    <img src="Pictures/fotopartido1.png">
+    <h4>partido1</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam error.</p>
+  <a href="#">Leer mas</a>
+  </div>
+
+  <div class="card">
+    <img src="Pictures/fotopartido1.png">
+    <h4>partido1</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam error.</p>
+  <a href="#">Leer mas</a>
+  </div>
+    
+  <div class="card">
+    <img src="Pictures/fotopartido1.png">
+    <h4>partido1</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam error.</p>
+  <a href="#">Leer mas</a>
+  </div>
+
+</div>
+
+
+<div class="container">
+  <div class="card">
+    <img src="Pictures/fotopartido1.png">
+    <h4>partido1</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam error.</p>
+  <a href="#">Leer mas</a>
+  </div>
+
+  <div class="card">
+    <img src="Pictures/fotopartido1.png">
+    <h4>partido1</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam error.</p>
+  <a href="#">Leer mas</a>
+  </div>
+    
+  <div class="card">
+    <img src="Pictures/fotopartido1.png">
+    <h4>partido1</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam error.</p>
+  <a href="#">Leer mas</a>
+  </div>
+</div>
+</div>
+
+<?php require 'footer.php'?>
 </body>
 <style>
+
 .titulo{  
         z-index: 1;
         position:absolute;
         top:25%;
         left:35%;
         font-family: Arial; 
-        font-size: 25px; 
+        font-size: 100%; 
         COLOR: black; 
         text-shadow: 0px 0px 9px #508AD3;}
 .slider{ 
-    position: absolute;
+    position: relative;
     top: 25%;
-    left:20%;
-    width:68%;
+    left:25%;
+    width:50%;
     overflow: hidden;
     z-index: 0;
   }
@@ -84,5 +142,78 @@
     100%{margin-left:-300%; }
 }
 
+
+html,body{
+  margin:0;
+  padding : 0;
+  
+}
+body{
+  width: 100%;
+  height: 100%;
+  font-family: sans-serif;
+  letter-spacing: 0.03em;
+  line-height:1.6;
+
+}
+
+.title{
+  text-align:center;
+  font-size:40px;
+  color:#6a6a6a;
+  margin-top:100px;
+  font-weight:100;
+}
+.container{
+  width: 100%;
+  max-width:1200px;
+  height: 430px;
+  display:flex;
+  flex-wrap:wrap;
+  justify-content:center;
+  margin:auto;
+}
+
+.container .card{
+  width: 330px;
+  height: 430px;
+  border-radius:8px;
+  box-shadow:0 2px 2px rgba(0,0,0,0.2);
+  overflow:hidden;
+  margin:20px;
+  text-align:center;
+  transition:all 0.25s;
+}
+
+.container .card:hover{
+  transform:translateY(-15px);
+  box-shadow: 0 12px 16px rgba(0, 0, 0, 0.2);
+}
+
+.container .card img{
+  width: 330px;
+  height: 220px;
+}
+
+.container .card h4{
+  font-weight:600;
+}
+
+.container .card p{
+  padding:0 1rem;
+  font-size: 16px;
+  font-weight:300;
+}
+
+.container .card a{
+  font-weight:300;
+  text-decoration: none;
+  color: #3498db;
+}
+
+.contenedorgrande{
+  margin-bottom: 300px;
+  position:relative;
+}
 
 </style>
--- futbolMasculino.php
+++ futbolMasculino.php
@@ -36,7 +36,65 @@
 </ul>
 </div>
 
+<title>Resumenes de los ultimos partidos</title>
+<div class="contendorgrande">
+<div class="container">
+  <div class="card">
+    <img src="Pictures/fotopartido1.png">
+    <h4>partido1</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam error.</p>
+  <a href="#">Leer mas</a>
+  </div>
+
+  <div class="card">
+    <img src="Pictures/fotopartido1.png">
+    <h4>partido1</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam error.</p>
+  <a href="#">Leer mas</a>
+  </div>
+    
+  <div class="card">
+    <img src="Pictures/fotopartido1.png">
+    <h4>partido1</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam error.</p>
+  <a href="#">Leer mas</a>
+  </div>
+
+</div>
+
+
+<div class="container">
+  <div class="card">
+    <img src="Pictures/fotopartido1.png">
+    <h4>partido1</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam error.</p>
+  <a href="#">Leer mas</a>
+  </div>
+
+  <div class="card">
+    <img src="Pictures/fotopartido1.png">
+    <h4>partido1</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam error.</p>
+  <a href="#">Leer mas</a>
+  </div>
+    
+  <div class="card">
+    <img src="Pictures/fotopartido1.png">
+    <h4>partido1</h4>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam error.</p>
+  <a href="#">Leer mas</a>
+  </div>
+</div>
+</div>
+
+<div><?php require 'footer.php'?></div>
+
 </body>
+
+
+
+
+
 
 <style>
 .titulo{  
@@ -49,7 +107,7 @@
         COLOR: black; 
         text-shadow: 0px 0px 9px #508AD3;}
 .slider{ 
-    position: absolute;
+    position: relative;
     top: 25%;
     left:20%;
     width:68%;
@@ -89,4 +147,78 @@
     100%{margin-left:-300%; }
 }
 
+
+
+html,body{
+  margin:0;
+  padding : 0;
+  
+}
+body{
+  width: 100%;
+  height: 100%;
+  font-family: sans-serif;
+  letter-spacing: 0.03em;
+  line-height:1.6;
+
+}
+
+.title{
+  text-align:center;
+  font-size:40px;
+  color:#6a6a6a;
+  margin-top:100px;
+  font-weight:100;
+}
+.container{
+  width: 100%;
+  max-width:1200px;
+  height: 430px;
+  display:flex;
+  flex-wrap:wrap;
+  justify-content:center;
+  margin:auto;
+}
+
+.container .card{
+  width: 330px;
+  height: 430px;
+  border-radius:8px;
+  box-shadow:0 2px 2px rgba(0,0,0,0.2);
+  overflow:hidden;
+  margin:20px;
+  text-align:center;
+  transition:all 0.25s;
+}
+
+.container .card:hover{
+  transform:translateY(-15px);
+  box-shadow: 0 12px 16px rgba(0, 0, 0, 0.2);
+}
+
+.container .card img{
+  width: 330px;
+  height: 220px;
+}
+
+.container .card h4{
+  font-weight:600;
+}
+
+.container .card p{
+  padding:0 1rem;
+  font-size: 16px;
+  font-weight:300;
+}
+
+.container .card a{
+  font-weight:300;
+  text-decoration: none;
+  color: #3498db;
+}
+
+.contenedorgrande{
+  margin-bottom: 300px;
+  position:relative;
+}
 </style>
